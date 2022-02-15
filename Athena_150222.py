72871eab20cf79c25f53f30d89f0b3e61cef4327
YugandharGantala <58016782+YugandharGantala@users.noreply.github.com>
2022-02-15 13:51:04 +0530

    Add files via upload

--- Athena_150222.py
+++ Athena_150222.py
@@ -75,26 +75,6 @@ def common_elements1(list1, list2):
 def common_elements2(list1, list2):
   return [x for x in list1 if x in list2]
 
-# nlp_en_core_web_sm = spacy.load('en_core_web_sm')
-
-# NER = spacy.load("en_core_web_sm")
-# def ner(text):
-#   NER = spacy.load("en_core_web_sm")
-#   text1 = NER(text)
-#   for word in text1.ents:
-#     print(word.text,word.label_)
-#   #displacy.render(text1,style="ent",jupyter=True)
-
-
-# def extract_named_ents(text):
-#     """Extract named entities, and beginning, middle and end idx using spaCy's out-of-the-box model. 
-    
-#     Keyword arguments:
-#     text -- the actual text source from which to extract entities
-    
-#     """
-#     return [(ent.text, ent.label_) for ent in nlp_en_core_web_sm(text).ents]
-
 def graph_controls(chart_type, df, dropdown_options):
     """
     Function which determines the widgets that would be shown for the different chart types
@@ -108,6 +88,8 @@ def graph_controls(chart_type, df, dropdown_options):
     length_of_options -= 1
 
     #plot = px.scatter()
+    if chart_type == 'None':
+        st.subheader("Select chart")
 
     if chart_type == 'Scatter plots':
         st.sidebar.subheader("Scatterplot Settings")
@@ -116,18 +98,6 @@ def graph_controls(chart_type, df, dropdown_options):
             x_values = st.sidebar.selectbox('X axis', index=length_of_options,options=dropdown_options)
             y_values = st.sidebar.selectbox('Y axis',index=length_of_options, options=dropdown_options)
             color_value = st.sidebar.selectbox("Color", index=length_of_options,options=dropdown_options)
-            # symbol_value = st.sidebar.selectbox("Symbol",index=length_of_options, options=dropdown_options)
-            # size_value = st.sidebar.selectbox("Size", index=length_of_options,options=dropdown_options)
-            # hover_name_value = st.sidebar.selectbox("Hover name", index=length_of_options,options=dropdown_options)
-            # facet_row_value = st.sidebar.selectbox("Facet row",index=length_of_options, options=dropdown_options,)
-            # facet_column_value = st.sidebar.selectbox("Facet column", index=length_of_options,
-            #                                           options=dropdown_options)
-            # marginalx = st.sidebar.selectbox("Marginal X", index=2,options=['rug', 'box', None,
-            #                                                              'violin', 'histogram'])
-            # marginaly = st.sidebar.selectbox("Marginal Y", index=2,options=['rug', 'box', None,
-            #                                                              'violin', 'histogram'])
-            # log_x = st.sidebar.selectbox('Log axis on x', options=[True, False])
-            # log_y = st.sidebar.selectbox('Log axis on y', options=[True, False])
             title = st.sidebar.text_input(label='Title of chart')
             plot = px.scatter(data_frame=df,
                               x=x_values,
@@ -135,17 +105,6 @@ def graph_controls(chart_type, df, dropdown_options):
                               color=color_value, title=title)
             st.subheader("Chart")
             st.plotly_chart(plot)
-            
-            # plot = px.scatter(data_frame=df,
-            #                   x=x_values,
-            #                   y=y_values,
-            #                   color=color_value,
-            #                   symbol=symbol_value,
-            #                   size=size_value,
-            #                   hover_name=hover_name_value,
-            #                   facet_row=facet_row_value,
-            #                   facet_col=facet_column_value,
-            #                   log_x=log_x, log_y=log_y,marginal_y=marginaly, marginal_x=marginalx, title=title)
 
         except Exception as e:
             print(e)
@@ -164,18 +123,6 @@ def graph_controls(chart_type, df, dropdown_options):
         except Exception as e:
             print(e)
 
-    # if chart_type == 'Bar Chart':
-    #     st.sidebar.subheader("Bar Settings")
-
-    #     try:
-    #         x_values = st.sidebar.selectbox('X axis', index=length_of_options,options=dropdown_options)
-    #         y_values = st.sidebar.selectbox('Y axis',index=length_of_options, options=dropdown_options)
-    #         color_value = st.sidebar.selectbox("Color", index=length_of_options,options=dropdown_options)
-    #         title = st.sidebar.text_input(label='Title of chart')
-    #         st.write(sns.barplot(data_frame=df, x=x_values, y=y_values, title=title))
-    #     except Exception as e:
-    #         print(e)
-
     if chart_type == 'Histogram':
         st.sidebar.subheader("Histogram Settings")
 
@@ -184,65 +131,30 @@ def graph_controls(chart_type, df, dropdown_options):
             y_values = st.sidebar.selectbox('Y axis',index=length_of_options, options=dropdown_options)
             nbins = st.sidebar.number_input(label='Number of bins', min_value=2, value=5)
             color_value = st.sidebar.selectbox("Color", index=length_of_options,options=dropdown_options)
-
             barmode = st.sidebar.selectbox('bar mode', options=['group', 'overlay','relative'], index=2)
-            # marginal = st.sidebar.selectbox("Marginal", index=2,options=['rug', 'box', None,
-            #                                                              'violin', 'histogram'])
-            # barnorm = st.sidebar.selectbox('Bar norm', options=[None, 'fraction', 'percent'], index=0)
-            # hist_func = st.sidebar.selectbox('Histogram aggregation function', index=0,
-            #                                  options=['count','sum', 'avg', 'min', 'max'])
-            # histnorm = st.sidebar.selectbox('Hist norm', options=[None, 'percent', 'probability', 'density',
-            #                                                       'probability density'], index=0)
-            # hover_name_value = st.sidebar.selectbox("Hover name", index=length_of_options,options=dropdown_options)
-            # facet_row_value = st.sidebar.selectbox("Facet row",index=length_of_options, options=dropdown_options,)
-            # facet_column_value = st.sidebar.selectbox("Facet column", index=length_of_options,
-            #                                           options=dropdown_options)
-            # cummulative = st.sidebar.selectbox('Cummulative', options=[False, True])
-            # log_x = st.sidebar.selectbox('Log axis on x', options=[True, False])
-            # log_y = st.sidebar.selectbox('Log axis on y', options=[True, False])
+  
             title = st.sidebar.text_input(label='Title of chart')
             plot = px.histogram(data_frame=df,barmode=barmode, x=x_values, y=y_values, color=color_value, title=title)
             st.subheader("Chart")
             st.plotly_chart(plot)
-            # plot = px.histogram(data_frame=df,barmode=barmode,histnorm=histnorm,
-            #                     marginal=marginal,barnorm=barnorm,histfunc=hist_func,
-            #                     x=x_values,y=y_values,cumulative=cummulative,
-            #                     color=color_value,hover_name=hover_name_value,
-            #                     facet_row=facet_row_value,nbins=nbins,
-            #                     facet_col=facet_column_value,log_x=log_x,
-            #                     log_y=log_y, title=title)
 
         except Exception as e:
             print(e)
 
-
     if chart_type == 'Box plots':
         st.sidebar.subheader('Box plot Settings')
-
         try:
             x_values = st.sidebar.selectbox('X axis', index=length_of_options, options=dropdown_options)
             y_values = st.sidebar.selectbox('Y axis', index=length_of_options, options=dropdown_options)
             color_value = st.sidebar.selectbox("Color", index=length_of_options, options=dropdown_options)
             boxmode = st.sidebar.selectbox('Violin mode', options=['group', 'overlay'])
             outliers = st.sidebar.selectbox('Show outliers', options=[False, 'all', 'outliers', 'suspectedoutliers'])
-            # hover_name_value = st.sidebar.selectbox("Hover name", index=length_of_options, options=dropdown_options)
-            # facet_row_value = st.sidebar.selectbox("Facet row", index=length_of_options, options=dropdown_options, )
-            # facet_column_value = st.sidebar.selectbox("Facet column", index=length_of_options,
-            #                                           options=dropdown_options)
-            # log_x = st.sidebar.selectbox('Log axis on x', options=[True, False])
-            # log_y = st.sidebar.selectbox('Log axis on y', options=[True, False])
-            # notched = st.sidebar.selectbox('Notched', options=[True, False])
             title = st.sidebar.text_input(label='Title of chart')
             plot = px.box(data_frame=df, x=x_values,
                           y=y_values, color=color_value,
                           boxmode=boxmode, points=outliers, title=title)
             st.subheader("Chart")
             st.plotly_chart(plot)
-            # plot = px.box(data_frame=df, x=x_values,
-            #               y=y_values, color=color_value,
-            #               hover_name=hover_name_value,facet_row=facet_row_value,
-            #               facet_col=facet_column_value, notched=notched,
-            #               log_x=log_x, log_y=log_y, boxmode=boxmode, points=outliers, title=title)
 
         except Exception as e:
             print(e)
@@ -254,8 +166,6 @@ def graph_controls(chart_type, df, dropdown_options):
         try:
             name_value = st.sidebar.selectbox(label='Name (Selected Column should be categorical)', options=dropdown_options)
             color_value = st.sidebar.selectbox(label='Color(Selected Column should be categorical)', options=dropdown_options)
-            # value = st.sidebar.selectbox("Value", index=length_of_options, options=dropdown_options)
-            # hole = st.sidebar.selectbox('Log axis on y', options=[True, False])
             title = st.sidebar.text_input(label='Title of chart')
 
             plot = px.pie(data_frame=df,names=name_value, color=color_value, title=title)
@@ -264,29 +174,19 @@ def graph_controls(chart_type, df, dropdown_options):
 
         except Exception as e:
             print(e)
-    # st.subheader("Chart")
-    # st.plotly_chart(plot)
-
 
     if chart_type == 'Heatmap':
         st.sidebar.subheader("Heatmap Settings")
 
         try:
             selected_columns=st.sidebar.multiselect("Select Preferred columns",dropdown_options)
-            #df1=df[selected_columns]
-            #y_values = st.sidebar.selectbox('Y axis', index=length_of_options, options=dropdown_options)
             title = st.sidebar.text_input(label='Title of chart')
             st.write(sns.heatmap(df[selected_columns].corr(), vmax=1, square=True, annot=True,cmap='viridis'))	
             st.subheader("Chart")
             st.pyplot()
-            # st.subheader("Chart")
-            # st.plotly_chart(plot)
 
         except Exception as e:
             print(e)
-    #st.pyplot()
-    # st.subheader("Chart")
-    # st.plotly_chart(plot)
 
 chart_types=['scatterplot', 'piechart', 'histogram']
 
@@ -294,12 +194,7 @@ def main():
   st.subheader('1. Upload the dataset')
   if st.checkbox("Upload File"):
     data=st.file_uploader("Upload file",type=['csv', 'excel'])
-    #text=st.text_area("Enter the text","Type Here ..")
-    # st.markdown("**Named Entity Recognition:**")
-    # if st.checkbox("Named Entities of input text"):
-    #     entity_result = extract_named_ents(text)
-    #     st.markdown(entity_result)
-    
+
     if data is not None:
         df=pd.read_csv(data)
         columns = list(df.columns)
@@ -307,8 +202,8 @@ def main():
         st.sidebar.subheader("Chart selection")
         chart_type = st.sidebar.selectbox(label="Select your chart type.",
                                                 options=['Scatter plots', 'Line Chart', 'Pie Charts',
-                                                        'Histogram', 'Box plots', 'Heatmap'])  # 'Line plots',
-  graph_controls(chart_type=chart_type, df=df, dropdown_options=columns)
+                                                        'Histogram', 'Box plots', 'Heatmap', 'None'])  # 'Line plots',
+        graph_controls(chart_type=chart_type, df=df, dropdown_options=columns)
   st.subheader('2. Enter text')
   if st.checkbox("Input text"):
     text=st.text_area("Enter the text","Type Here ..")
@@ -372,3 +267,4 @@ def main():
 
 if __name__ == '__main__':
 	main()  
+
